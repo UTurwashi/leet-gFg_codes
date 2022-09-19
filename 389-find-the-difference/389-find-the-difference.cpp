8494b0c12ed5fab34e5e3a96f85d3669755123f4
Urwashi Tiwari <87020342+UTurwashi@users.noreply.github.com>
2022-09-19 23:24:51 +0530

    Time: 7 ms (40.84%), Space: 7 MB (9.29%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
             s=s+t;
-           int c=0;
+           char c=0;
             for(auto i:s)
                 c=c^i;
             return c;
